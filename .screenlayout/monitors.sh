08f44a3659c8b0d08ea66b6af03b553519501c21
Aleksa Petrovic <aleksapetrovic99@gmail.com>
2022-12-03 01:31:43 +0000

    Added picom config, added picom to xinitrc, catppuccin alacritty, fixed monitor layout

--- .screenlayout/monitors.sh
+++ .screenlayout/monitors.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DVI-D-1 --mode 1600x900 --pos 0x180 --rotate normal --output HDMI-1 --primary --mode 1920x1080 --pos 1600x0 --rotate normal --output DP-1 --mode 1366x768 --pos 3520x312 --rotate normal
+xrandr --output DVI-D-0 --mode 1600x900 --pos 0x180 --rotate normal --output HDMI-0 --primary --mode 1920x1080 --pos 1600x0 --rotate normal --output DP-0 --mode 1366x768 --pos 3520x312 --rotate normal --output DP-1 --off
