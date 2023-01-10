5b68c9cf6626d081be3ac8052d0d6e03b1c2841b
Aleksa Petrovic <aleksapetrovic99@gmail.com>
2023-01-10 04:21:47 +0100

    added scala, astro support, added nvim tree refresh

--- .config/nvim/lua/packerplugins.lua
+++ .config/nvim/lua/packerplugins.lua
@@ -5,25 +5,29 @@ return require('packer').startup(function(use)
   use 'windwp/nvim-autopairs'
   use 'kyazdani42/nvim-web-devicons'
   use 'kyazdani42/nvim-tree.lua'
-  use 'iamcco/markdown-preview.nvim'
   use 'kevinhwang91/nvim-ufo'
   use 'lambdalisue/suda.vim'
   use 'nvim-lua/plenary.nvim'
+  use({
+    "iamcco/markdown-preview.nvim",
+    run = function() vim.fn["mkdp#util#install"]() end,
+  })
   use 'nvim-telescope/telescope.nvim'
   use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
   use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
   use 'williamboman/nvim-lsp-installer'
   use 'neovim/nvim-lspconfig'
-  use { 'ms-jpq/coq_nvim', branch='coq' }
-  use { 'ms-jpq/coq.artifacts', branch='artifacts' }
+  use { 'ms-jpq/coq_nvim', branch = 'coq' }
+  use { 'ms-jpq/coq.artifacts', branch = 'artifacts' }
   use 'mfussenegger/nvim-jdtls'
   use 'nvim-lualine/lualine.nvim'
-  use 'drewtempelmeyer/palenight.vim'
   use { "catppuccin/nvim", as = "catppuccin" }
+  use 'drewtempelmeyer/palenight.vim'
   use {
     'goolord/alpha-nvim',
-    config = function ()
-        require'alpha'.setup(require'alpha.themes.dashboard'.config)
+    config = function()
+      require 'alpha'.setup(require 'alpha.themes.dashboard'.config)
     end
   }
+  use({ 'scalameta/nvim-metals', requires = { "nvim-lua/plenary.nvim" } })
 end)
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,22 +1,22 @@
 require("nvim-lsp-installer").setup({
-    automatic_installation = true,
-    ui = {
-        icons = {
-            server_installed = "✓",
-            server_pending = "➜",
-            server_uninstalled = "✗"
-        }
+  automatic_installation = true,
+  ui = {
+    icons = {
+      server_installed = "✓",
+      server_pending = "➜",
+      server_uninstalled = "✗"
     }
+  }
 })
 
 require("nvim-autopairs").setup()
 
-require'nvim-web-devicons'.setup {
- color_icons = true;
- default = true;
+require 'nvim-web-devicons'.setup {
+  color_icons = true;
+  default = true;
 }
 
-require("lualine").setup{
+require("lualine").setup {
   options = {
     icons_enabled = true,
     theme = 'auto',
@@ -25,7 +25,6 @@ require("lualine").setup{
 
 local alpha = require("alpha")
 local dashboard = require("alpha.themes.dashboard")
-
 
 require('telescope').setup {
   extensions = {
@@ -36,21 +35,20 @@ require('telescope').setup {
       case_mode = "smart_case",
     }
   },
-	pickers = {
-		find_files = {
-			hidden = true,
-		}
-	}
+  pickers = {
+    find_files = {
+      hidden = true,
+    }
+  }
 }
 
 require('telescope').load_extension('fzf')
 
-require'nvim-treesitter.configs'.setup {
-	  ensure_installed = { "java", "typescript", "python", "go" },
-		highlight = {
-			enable = true,
-		},
+require 'nvim-treesitter.configs'.setup {
+  ensure_installed = { "java", "typescript", "javascript", "python", "go", "css", "tsx", "html" },
+  highlight = {
+    enable = true,
+  },
 }
 
-require("nvim-tree").setup()
-
+require("nvim-tree").setup({ update_focused_file = { enable = true, }, })
